18c6d75d30c69c48de9570fa146e14831ecf4536
sidney-earthshot <sidney.pang@earthshot.ca>
2024-01-03 16:45:01 -0500

    formatted top box, added vertical line and image

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,43 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center w-fit">
+      <div className="flex flex-col items-center">
+        {/* top box */}
         <div className="bg-blue-400 p-5 w-[450px] mt-5 rounded-xl">
-          <p className="text-white">🌳BAD OUTCOME #1</p>
-          <p className="text-white text-3xl">900M = malnutrition.</p>
-          <p className="text-white">
+          <p className="text-white text-sm">🌳FOREST & ☂️UMBRELLA #1</p>
+          <p className="text-white text-3xl my-1">Bad water & sanitation.</p>
+          <p className="text-white mb-2">
             Globally, 1 in 3 ≠ safe drinking water, mostly ≠ LDC's
           </p>
 
-          {/* 3 green buttons */}
-          <div className="flex">
-            <Risk basis={"4/12"} content={"HIGH..."} />
-            <Risk basis={"4/12"} content={"HIGH..."} />
-            <Risk basis={"3/12"} content={"HIGH..."} />
-          </div>
-
-          <p className="text-white text-xl">SUMMARY</p>
           <p className="text-white">{`😮Inaction cost: {xx}, {yy} QALY's, {zz} monetary`}</p>
           <p className="text-white">{`💰Cost to solve problem now: {xx} people, {yy} invenestment, {zz} opportunity cost v. others.`}</p>
           <p className="text-white">{`💰Opportunity for innovation. Reduce cost by {xx%} by fixing {1, 2, 3}, improving ROI by {yy} per $ + hr spent.`}</p>
           <p className="text-white">
             🌎Gap: Technologies $ operational risk. High CapEx.
           </p>
+
+          {/* 3 green buttons */}
+          <div className="flex">
+            <Risk basis={"basis-4/12"} content={"HIGH..."} />
+            <Risk basis={"basis-4/12"} content={"HIGH..."} />
+            <Risk basis={"basis-3/12"} content={"HIGH..."} />
+          </div>
+        </div>
+
+        {/* divider */}
+        <div className="h-[300px] bg-gray-700 w-[3px]"></div>
+        {/* <img
+          src={`https://plus.unsplash.com/premium_photo-1683836722388-8643468c327d?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
+          alt="world"
+          className="w-[450px] h-[150px] absolute mt-[450px] rounded-lg"
+        />
+
+        <p className="text-left text-white text-3xl w-[450px] pl-5 absolute mt-[510px]">By country.</p> */}
+        <div
+          className={`bg-[url("https://plus.unsplash.com/premium_photo-1683836722388-8643468c327d?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] w-[450px] h-[110px] bg-cover flex items-center rounded-lg`}
+        >
+          <p className="text-white text-3xl pl-6 font-bold">By country.</p>
         </div>
       </div>
     </>
--- src/assets/components/Risk.jsx
+++ src/assets/components/Risk.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../../index.css"
 
 export default function Risk({ basis, content }) {
   return (
     <div
-      className={`text-white text-sm bg-green-700 basis-${basis} px-3 py-1 mr-4 my-2 rounded-full`}
+      className={`text-white text-sm bg-green-700 ${basis} px-3 py-1 mr-4 mt-6 rounded-full`}
     >
       {content}
     </div>
