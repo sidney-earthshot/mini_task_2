b89d62b8dd0d7605f3719b6bcebbcb83c0157286
sidney-earthshot <sidney.pang@earthshot.ca>
2024-01-04 13:34:49 -0500

    finished layout and colours

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center w-fit xl:w-full">
         {/* top box */}
         <div className="bg-blue-400 p-5 w-[450px] mt-5 rounded-xl">
           <p className="text-white text-sm">🌳FOREST & ☂️UMBRELLA #1</p>
@@ -41,12 +41,105 @@ function App() {
 
         {/* divider */}
         <div className="h-[150px] bg-gray-700 w-[3px]"></div>
+
         {/* div to adjust the intersection point of all 3 lines */}
-        <div className="flex mt-[-50px]">
-          <div className="h-[5px] bg-sky-700 w-[400px] -skew-y-[10deg] "></div>
-          <div className="h-[5px] bg-sky-700 w-[400px] skew-y-[10deg] "></div>
+        <div className="flex mt-[-40px]">
+          {/* 2 skewed lines */}
+          <div className="h-[5px] bg-sky-700 w-[550px] -skew-y-[10deg]"></div>
+          <div className="h-[5px] bg-sky-700 w-[550px] skew-y-[10deg]"></div>
         </div>
-        <div className="h-[100px] bg-gray-700 w-[1000px] mt-[20px]"></div>
+
+        {/* bottom div for boxes */}
+        <div className="h-[100px] my-[30px] flex z-10 [&>*]:w-[450px] space-x-3 [&>*]:p-5 [&>*]:h-fit">
+          <div className="bg-blue-400 rounded-lg">
+            <p className="text-white text-sm">🌳TREE 1.1 & ☂️UMBRELLA 1.1</p>
+            <p className="text-white text-2xl">40m in Ethiopia.</p>
+            <p className="text-white text-sm">
+              Causing XX deaths, YY reduction in QALY's, ZZ $cost.
+            </p>
+
+            <div className="bg-neutral-800 p-6 rounded-lg mt-4">
+              <p className="text-white font-bold text-xl">problem profile.</p>
+            </div>
+
+            <div className="flex">
+              <Risk
+                basis={"basis-4/12"}
+                content={"HIGH..."}
+                bg_colour={"bg-green-700"}
+              />
+              <Risk
+                basis={"basis-4/12"}
+                content={"HIGH..."}
+                bg_colour={"bg-green-700"}
+              />
+              <Risk
+                basis={"basis-3/12"}
+                content={"HIGH..."}
+                bg_colour={"bg-green-700"}
+              />
+            </div>
+          </div>
+          <div className="bg-blue-400 rounded-lg">
+            <p className="text-white text-sm">🌳TREE 1.2 & ☂️UMBRELLA 1.2</p>
+            <p className="text-white text-2xl">32m in Afghanistan.</p>
+            <p className="text-white text-sm">
+              Causing XX deaths, YY reduction in QALY's, ZZ $cost.
+            </p>
+
+            <div className="bg-neutral-800 p-6 rounded-lg mt-4">
+              <p className="text-white font-bold text-xl">problem profile.</p>
+            </div>
+
+            <div className="flex">
+              <Risk
+                basis={"basis-4/12"}
+                content={"MED SOLVABILITY"}
+                bg_colour={"bg-yellow-500"}
+              />
+              <Risk
+                basis={"basis-4/12"}
+                content={"MED SOLVABILITY"}
+                bg_colour={"bg-yellow-500"}
+              />
+              <Risk
+                basis={"basis-3/12"}
+                content={"HIGH IMPACT"}
+                bg_colour={"bg-green-700"}
+              />
+            </div>
+          </div>
+
+          <div className="bg-blue-400 rounded-lg">
+            <p className="text-white text-sm">🌳TREE 1.2 & ☂️UMBRELLA 1.3</p>
+            <p className="text-white text-2xl">163m in india.</p>
+            <p className="text-white text-sm">
+              Causing XX deaths, YY reduction in QALY's, ZZ $cost.
+            </p>
+
+            <div className="bg-neutral-800 p-6 rounded-lg mt-4">
+              <p className="text-white font-bold text-xl">problem profile.</p>
+            </div>
+
+            <div className="flex">
+              <Risk
+                basis={"basis-4/12"}
+                content={"HIGH..."}
+                bg_colour={"bg-green-700"}
+              />
+              <Risk
+                basis={"basis-4/12"}
+                content={"HIGH..."}
+                bg_colour={"bg-green-700"}
+              />
+              <Risk
+                basis={"basis-3/12"}
+                content={"HIGH..."}
+                bg_colour={"bg-green-700"}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/assets/components/Risk.jsx
+++ src/assets/components/Risk.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../../index.css"
 
-export default function Risk({ basis, content }) {
+export default function Risk({ basis, content, bg_colour }) {
   return (
     <div
-      className={`text-white text-sm bg-green-700 ${basis} px-3 py-1 mr-4 mt-6 rounded-full`}
+      className={`text-white text-xs ${bg_colour} ${basis} px-3 py-1 mr-4 mt-6 rounded-full`}
     >
       {content}
     </div>
